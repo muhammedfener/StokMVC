89f2bc8a64e764855826213ac659d2438be1c093
muhammedfener <muhammedfener97@gmail.com>
2023-03-19 19:44:50 +0300

    Add project files.

---
+++ MVCStok/Controllers/KategoriController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCStok.Models.Entity;
+
+namespace MVCStok.Controllers
+{
+    public class KategoriController : Controller
+    {
+        DB_MVCStokEntities db = new DB_MVCStokEntities();
+
+        // GET: Kategori
+        public ActionResult Index()
+        {
+            var kategoriler = db.TBLKategori.ToList();
+            return View(kategoriler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniKategori()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniKategori(TBLKategori p)
+        {
+            db.TBLKategori.Add(p);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriSil(int id)
+        {
+            var kategori = db.TBLKategori.Find(id);
+            db.TBLKategori.Remove(kategori);
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir(int id)
+        {
+            var kategori = db.TBLKategori.Find(id);
+
+            return View(kategori);
+        }
+
+        [HttpPost]
+        public ActionResult KategoriGuncelle(TBLKategori p)
+        {
+            var kategori = db.TBLKategori.Find(p.id);
+            kategori.ad = p.ad;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCStok/Controllers/UrunController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Web;
+using System.Web.Mvc;
+using MVCStok.Models.Entity;
+
+namespace MVCStok.Controllers
+{
+    public class UrunController : Controller
+    {
+        DB_MVCStokEntities db = new DB_MVCStokEntities();
+        // GET: Urun
+        public ActionResult Index()
+        {
+            var urunler = db.TBLUrunler.ToList();
+            return View(urunler);
+        }
+
+        [HttpGet]
+        public ActionResult YeniUrun()
+        {
+            List<SelectListItem> kategori = (from x in db.TBLKategori.ToList() 
+                                             select new SelectListItem 
+                                             { 
+                                                 Value = x.id.ToString(), 
+                                                 Text = x.ad 
+                                             }).ToList();
+
+            ViewBag.drop = kategori;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult YeniUrun(TBLUrunler p)
+        {
+            var kategori = db.TBLKategori.Where(x => x.id == p.kategori).FirstOrDefault();
+            p.TBLKategori = kategori;
+            db.TBLUrunler.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult UrunGetir(int id)
+        {
+            var urun = db.TBLUrunler.Find(id);
+            return View(urun);
+        }
+    }
+}
