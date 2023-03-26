50b58f1faa3204355de114ad90aee55e6594412b
muhammedfener <muhammedfener97@gmail.com>
2023-03-26 19:43:27 +0300

    .

---
+++ MVCStok/Controllers/AdminController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCStok.Models.Entity;
+
+namespace MVCStok.Controllers
+{
+    public class AdminController : Controller
+    {
+        DB_MVCStokEntities db = new DB_MVCStokEntities();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet] 
+        public ActionResult YeniAdmin() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniAdmin(TBLAdmin admin)
+        {
+            db.TBLAdmin.Add(admin);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MVCStok/Controllers/GirisYapController.cs
@@ -0,0 +1,34 @@
+﻿using MVCStok.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace MVCStok.Controllers
+{
+    public class GirisYapController : Controller
+    {
+        DB_MVCStokEntities db = new DB_MVCStokEntities();
+
+        // GET: GirisYap
+        public ActionResult Giris()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Giris(TBLAdmin t)
+        {
+            var bilgiler = db.TBLAdmin.FirstOrDefault(x => x.kullanici == t.kullanici && x.sifre == t.sifre);
+            if(bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.kullanici, false);
+                return RedirectToAction("Index", "Musteri");
+            }
+
+            return View();
+        }
+    }
+}
